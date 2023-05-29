aba6457ad8c82bd91555eb130dd6c44e056cba90
botayeva <botayeva.t@gmail.com>
2023-05-29 22:41:43 +0600

    changed message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Tbilisi!");
+        System.out.println("Hello, Tbilisi!!!!!!");
     }
 }
