a47439a19ffd082e91244fae98c25a2793da8da3
botayeva <botayeva.t@gmail.com>
2023-05-29 23:14:23 +0600

    var

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Tbilisi!!!!!!");
+        String message = "Hello, Tbilisi!!!!!!";
+        System.out.println(message);
     }
 }
